bf4170a2b3b905cdea13b8d3939b6687aa15adbd
rfancn <reg_info@qq.com>
2024-10-17 11:20:40 +0800

    add excel help functions

--- excel/excel_sheet.go
+++ excel/excel_sheet.go
@@ -36,11 +36,37 @@ func (r *SheetRow) GetInt64(colName string) int64 {
 	return cast.ToInt64(r.Get(colName))
 }
 
+func (r *SheetRow) GetInt32(colName string) int32 {
+	return cast.ToInt32(r.Get(colName))
+}
+
+func (r *SheetRow) GetInt(colName string) int {
+	return cast.ToInt(r.Get(colName))
+}
+
+func (r *SheetRow) GetFloat64(colName string) float64 {
+	return cast.ToFloat64(r.Get(colName))
+}
+
 // GetInt64Slice get comma separated int64 slice
 func (r *SheetRow) GetInt64Slice(colName string) []int64 {
 	v := r.Get(colName)
 	v = strings.ReplaceAll(v, "，", ",")
 	return convert.CsvToInt64s(v)
+}
+
+// GetInt32Slice get comma separated int32 slice
+func (r *SheetRow) GetInt32Slice(colName string) []int32 {
+	v := r.Get(colName)
+	v = strings.ReplaceAll(v, "，", ",")
+	return convert.CsvToInt32s(v)
+}
+
+// GetIntSlice get comma separated int slice
+func (r *SheetRow) GetIntSlice(colName string) []int {
+	v := r.Get(colName)
+	v = strings.ReplaceAll(v, "，", ",")
+	return convert.CsvToInts(v)
 }
 
 // GetStringSlice get comma separated string slice
